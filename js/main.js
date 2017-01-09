704f5504707048da6eb177f16c55196b5565afac
Bohdan Tsusbylskyy <b.tsybuslkyy@gmail.com>
2017-01-09 16:24:26 +0200

    some fix

--- js/main.js
+++ js/main.js
@@ -1,6 +1,33 @@
+function slider(){
+    console.log('slider start');
+    var items = $('.members-slider li');
+    var actives = [];
+    var next;
+    var prev;
+    for(var i = 0; i < items.length; i++){
+        if($(items[i]).css('display') != 'none'){
+            actives.push(items[i]);
+        }
+    }
+    next = actives.length % items.length;
+    prev = (actives.length + 1) % items.length;
+    if($(this).hasClass('right')){
+        $(actives[actives.length-1]).addClass('hide');
+        $(items[actives.length-1]).removeClass('hide');
+        $(items[next]).prependTo(".members-slider ul");
+    }else if($(this).hasClass('left')){
+        $(actives[0]).addClass('hide');
+        $(items[prev]).removeClass('hide');
+        $(items[prev]).appendTo(".members-slider ul");
+    }
+    console.log(actives);
+
+};
+
 $(function(){
     $('#hamb').on('click',function(){
         $('.menu').toggleClass('show');
         $('.menu li').toggleClass('show');
     });
+    $('.angle').on('click',slider);
 });
