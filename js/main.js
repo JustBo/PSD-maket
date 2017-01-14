6818f3b122920f6aaac9b4878831b1c02380cd05
Bohdan Tsusbylskyy <b.tsybuslkyy@gmail.com>
2017-01-14 23:17:09 +0200

    many fixes)

--- js/main.js
+++ js/main.js
@@ -1,44 +1,51 @@
-function slider(){
-    console.log('slider start');
-    var items = $('.members-slider li');
-    var actives = [];
-    var next;
-    var prev;
-    for(var i = 0; i < items.length; i++){
-        if($(items[i]).css('display') != 'none'){
-            actives.push(items[i]);
-        }
-    }
-    next = actives.length % items.length;
-    prev = (actives.length + 1) % items.length;
-    if($(this).hasClass('right')){
-        $(actives[actives.length-1]).addClass('hide');
-        $(items[actives.length-1]).removeClass('hide');
-        $(items[next]).prependTo(".members-slider ul");
-    }else if($(this).hasClass('left')){
-        $(actives[0]).addClass('hide');
-        $(items[prev]).removeClass('hide');
-        $(items[prev]).appendTo(".members-slider ul");
-    }
-    console.log(actives);
-
-};
-
 $(function(){
     $('#hamb').on('click',function(){
         $('.menu').toggleClass('show');
-        $('.menu li').toggleClass('show');
+        $('.menu').addClass('animated fadeInDown');
     });
-    $('.angle').on('click',slider);
     $('.works-items .item').hover( function(){
-        $(this).children().removeClass('hide');
+        $(this).find('.work-info').removeClass('hide');
+        $(this).find('.work-info').addClass('animated zoomIn');
     }, function(){
-        $(this).children().addClass('hide');
-    });
-
+        $(this).find('.work-info').addClass('hide');
+    })
     $('.blog-item').hover( function(){
         $(this).find('.hover').addClass('red');
     }, function(){
         $(this).find('.hover').removeClass('red');
+    });
+    $('.slides').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: $('#left'),
+        nextArrow: $('#right'),
+        responsive:[{
+            breakpoint:1212,
+            settings:{
+                slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint:920,
+            settings:{
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint:570,
+            settings:{
+                slidesToShow: 1,
+            }
+        }],
+    });
+    $('.quote-item').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        
+        autoplaySpeed: 2000,
+        arrows:false,
+        dots:true,
     });
 });
